440602f94e9d3388b3cf34df8b7b60af5f70fea9
Rahul Nallappa <you@example.com>
2017-05-03 12:57:02 -0500

    everything but jquery working

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+var friendsArray = [
+	{
+	name: "Corey", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores: [5, 1, 4, 4, 5, 1, 1, 5, 4, 1]
+	},
+	{
+	name: "John", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores: [1, 3, 7, 8, 2, 4, 5, 4, 1, 2]
+	},
+	{
+	name: "Frankie", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores: [7, 9, 4, 4, 7, 9, 1, 7, 4, 9]
+	},
+	{
+	name: "Naveen", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/9bd/3435aa3.jpg",
+	scores: [7, 9, 4, 4, 7, 9, 1, 7, 4, 9]
+	},
+	{
+	name: "Sam", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/9bd/3435aa3.jpg",
+	scores: [7, 6, 4, 4, 7, 9, 1, 7, 4, 9]
+	},
+	{
+	name: "Henry", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/9bd/3435aa3.jpg",
+	scores: [7, 9, 1, 4, 7, 9, 1, 7, 4, 9]
+	},
+	{
+	name: "Isaac", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/9bd/3435aa3.jpg",
+	scores: [7, 9, 4, 1, 7, 9, 1, 7, 4, 9]
+	},
+	{
+	name: "Anthony", 
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/9bd/3435aa3.jpg",
+	scores: [7, 9, 4, 1, 5, 9, 1, 7, 4, 9]
+	}
+]
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
+
